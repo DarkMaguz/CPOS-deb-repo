2bf6d52f02b54850ec62b6875fba0ce11b94e1b6
Peter <peter.balling@gmail.com>
2022-02-15 18:15:00 +0100

    Fix docker tag

--- initialize-repo.sh
+++ initialize-repo.sh
@@ -1,18 +1,18 @@
 #!/bin/sh -e
 
-docker build -t darkmagus/CPOS-deb-repo .
+docker build -t darkmagus/cpos-deb-repo .
 
 mkdir -p gnupg
 mkdir -p repo
 
 docker run -it --rm \
   -v $(pwd)/gnupg:/root/.gnupg \
-  darkmagus/CPOS-deb-repo \
+  darkmagus/cpos-deb-repo \
   gpg --full-generate-key
 exit
 
 docker run -it --rm \
   -v $(pwd)/repo:/root/repo/ \
   -v $(pwd)/gnupg:/root/.gnupg \
-  darkmagus/CPOS-deb-repo \
+  darkmagus/cpos-deb-repo \
   gpg --armor --export B4A0F522493B169B > repo/Release.gpg
