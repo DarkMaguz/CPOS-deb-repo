51a8eb6e6dca4f30958b05f91cc1f34cc9bf7cb2
Peter <peter.balling@gmail.com>
2022-02-20 11:59:20 +0100

    Provide PASS

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -4,6 +4,7 @@ docker run -ti --rm \
   -v $(pwd)/repo:/home/cpos/repo/ \
   -v $(pwd)/gnupg:/home/cpos/.gnupg \
   -v $(pwd)/docker-run.sh:/home/cpos/docker-run.sh \
+  -e PASS=$PASS \
   -e KEYID=$KEYID \
   darkmagus/cpos-deb-repo \
   ./docker-run.sh
