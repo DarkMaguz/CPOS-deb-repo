58957c3dad221cd8c5a1749107902e4d6054e2e1
Peter <peter.balling@gmail.com>
2022-02-28 15:24:41 +0100

    Removed interactive terminal arguments in refresh-repo script

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-docker run -ti --rm \
+docker run --rm \
   -v $(pwd)/repo:/home/cpos/repo/ \
   -v $(pwd)/gnupg:/home/cpos/.gnupg \
   -v $(pwd)/docker-run.sh:/home/cpos/docker-run.sh \
