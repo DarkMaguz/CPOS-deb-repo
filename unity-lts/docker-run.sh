eaf8fdead6e151982c52ef913a5d14ed3c7981aa
Peter <peter.balling@gmail.com>
2022-03-06 23:55:41 +0100

    Limit CURRENT_VERSION var to one line
    This makes the assumption that `apt-cache show` always will output
    the latest version available

--- unity-lts/docker-run.sh
+++ unity-lts/docker-run.sh
@@ -39,7 +39,7 @@ echo "Latest version: $LATEST_VERSION"
 
 # Get current version.
 apt-get update -y
-CURRENT_VERSION=$(apt-cache show unity-lts 2> /dev/null | grep "Version:" | cut -d' ' -f2 || echo "")
+CURRENT_VERSION=$(apt-cache show unity-lts 2> /dev/null | grep --max-count 1 "Version:" | cut -d' ' -f2)
 echo "Current version: $CURRENT_VERSION"
 
 # Check if we have the latest version.
