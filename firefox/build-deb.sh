d4ef1427540eb1dff92f956b83892ab631c5d81e
Peter Balling <peter.balling@gmail.com>
2025-03-11 21:20:58 +0100

    Updated firefox Dockerfile

--- firefox/build-deb.sh
+++ firefox/build-deb.sh
@@ -2,4 +2,9 @@
 
 docker build -t darkmagus/build-firefox-deb .
 
-docker run --rm -e USERID=$(id -u $USER) -e GROUPID=$(id -g $USER) -v $(pwd)/:/build darkmagus/build-firefox-deb
+docker run \
+  --rm \
+  -e USERID=$(id -u $USER) \
+  -e GROUPID=$(id -g $USER) \
+  -v ./:/build \
+  darkmagus/build-firefox-deb
