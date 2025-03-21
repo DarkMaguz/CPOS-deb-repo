b0ae567cfbc875be745949293e227aa074782616
Peter Balling <peter.balling@gmail.com>
2025-03-21 01:03:23 +0100

    Congregated build-deb shell scripts into one generic

---
+++ build-deb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+REPO_DIR="$2"
+BUILD_DIR="$1"
+IMAGE_NAME="darkmagus/build-$BUILD_DIR"
+
+cd $BUILD_DIR
+docker build -t $IMAGE_NAME .
+
+docker run \
+  --rm \
+  -e USERID=$(id -u $USER) \
+  -e GROUPID=$(id -g $USER) \
+  -v $(pwd)/:/build \
+  -v "$REPO_DIR:/deb" \
+  $IMAGE_NAME
--- discord/build-deb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-
-docker build -t darkmagus/build-discord .
-
-docker run --rm -e USERID=$(id -u $USER) -e GROUPID=$(id -g $USER) -v $(pwd)/:/build darkmagus/build-discord
